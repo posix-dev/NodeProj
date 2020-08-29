d113bfd97b98f08867bd20ef236930de6715e054
pashko00710 <pashko00710@yandex.ru>
2020-08-29 14:53:10 +0300

    rewrite code for async/await

--- app.js
+++ app.js
@@ -7,108 +7,100 @@ const sourceFolderName = config.sourceFolderName,
     targetFolderName = config.targetFolderName,
     rootFolderPath = config.rootFolderPath;
 
-fs.readdir(rootFolderPath, (err, files) => {
-    if (err || files.indexOf(sourceFolderName) === -1) {
-        console.log('Путь к исходной папке указан неверно');
-        return
-    }
-
-    //create target folder and search in source folder
-    fs.mkdir(targetFolderName, {recursive: true}, err => {
-        if (err) return;
-
-        recursiveSearch(sourceFolderName);
-    })
-});
-
-const recursiveSearch = function (directoryName) {
-    fs.readdir(directoryName, (err, files) => {
-        if (err) return;
-
-        files.forEach(item => {
-            let childPath = path.join(directoryName, item);
-
-            fs.stat(childPath, (err, stats) => {
-                if (err) return;
-
-                if (stats.isDirectory()) {
-                    recursiveSearch(childPath);
-                } else {
-                    moveToTargetFolder(childPath);
-                }
-            })
-        })
-    })
-};
-
-const moveToTargetFolder = function (filePath) {
-    let subFolderName = path.basename(filePath)[0].toUpperCase();
-
-    fs.readdir(targetFolderName, (err, files) => {
-        if (err) return;
-
-        let targetDirectory = path.join(targetFolderName, subFolderName);
-        if (files.indexOf(subFolderName) === -1) {
-            fs.mkdir(targetDirectory, err => {
-                if (err) return;
-                moveFile(filePath, targetDirectory);
-            });
-        } else {
-            moveFile(filePath, targetDirectory);
+const startMovement = async () => {
+    try {
+        let files = fs.readdirSync(rootFolderPath)
+        if (files && files.indexOf(sourceFolderName) > -1) {
+            fs.mkdirSync(targetFolderName, {recursive: true});
+            await recursiveSearch(sourceFolderName)
+        } else if (files.indexOf(sourceFolderName) === -1) {
+            console.log('Путь к исходной папке указан неверно');
+            throw new Error('Путь к исходной папке указан неверно');
         }
+    } catch (e) {
+        console.log(e)
+        throw new Error(e)
+    }
+};
 
+const recursiveSearch = async (directoryName) => {
+    let files = fs.readdirSync(directoryName);
+
+    files.forEach(item => {
+        let childPath = path.join(directoryName, item);
+        let info = fs.statSync(childPath);
+
+        if (info.isDirectory()) {
+            recursiveSearch(childPath);
+        } else {
+            moveToTargetFolder(childPath);
+        }
     })
 };
 
-function moveFile(sourcePath, targetDirectory) {
+const moveToTargetFolder = async (filePath) => {
+    let subFolderName = path.basename(filePath)[0].toUpperCase();
+    let files = fs.readdirSync(targetFolderName);
+    let targetDirectory = path.join(targetFolderName, subFolderName);
+
+    if (files.indexOf(subFolderName) === -1) {
+        fs.mkdirSync(targetDirectory);
+        await moveFile(filePath, targetDirectory);
+    } else {
+        await moveFile(filePath, targetDirectory);
+    }
+};
+
+const moveFile = async (sourcePath, targetDirectory) => {
     let fileName = path.basename(sourcePath),
         readStream = fs.createReadStream(sourcePath),
         writeStream = fs.createWriteStream(path.join(targetDirectory, fileName));
 
     readStream.on('error', err => {
+        throw new Error('readStream error')
     });
     writeStream.on('error', err => {
+        throw new Error('writeStream error')
     });
 
     if (isDelete) {
         readStream.on('close', (err) => {
-            if (err) return;
+            if (err) throw Error('moveFile close error');
 
-            fs.unlink(sourcePath, err => {
-                if (err) return;
-
-                deleteFolder();
-            });
+            fs.unlinkSync(sourcePath);
+            deleteFolder();
         });
     }
 
     readStream.pipe(writeStream);
-}
-
-
-const deleteFolder = function (pathToDeleteFolder = sourceFolderName) {
-    fs.readdir(pathToDeleteFolder, (err, files) => {
-        if (err) return;
-
-        if (files.length) {
-            files.forEach(item => deleteFolder(path.join(pathToDeleteFolder, item)));
-        } else {
-            fs.stat(pathToDeleteFolder, (err, stats) => {
-                if (err || !stats.isDirectory()) return;
-
-                fs.rmdir(pathToDeleteFolder, err => {
-                    if (err) return;
-
-                    if (path.dirname(pathToDeleteFolder) === sourceFolderName) {
-                        fs.rmdir(sourceFolderName, err => {
-                        })
-                    } else {
-                        deleteFolder(path.dirname(pathToDeleteFolder))
-                    }
-                })
-            })
-        }
-    })
 };
+
+const deleteFolder = async (pathToDeleteFolder = sourceFolderName) => {
+    let stats = fs.statSync(pathToDeleteFolder);
+    let files;
+
+    if (stats.isDirectory()) {
+        files = fs.readdirSync(pathToDeleteFolder);
+    }
+
+    if (!files) return;
+
+    if (files.length) {
+        files.forEach(item => deleteFolder(path.join(pathToDeleteFolder, item)));
+    } else {
+        fs.rmdirSync(pathToDeleteFolder, {recursive: true});
+        if (
+            path.dirname(pathToDeleteFolder) === sourceFolderName
+            && fs.readdirSync(sourceFolderName).length === 0
+        )
+            fs.rmdirSync(sourceFolderName);
+        else
+            await deleteFolder(path.dirname(pathToDeleteFolder));
+    }
+};
+
+startMovement()
+    .then(() => console.log('Everything is ok'))
+    .catch(e => console.log(`Error: ${e.message} - ${e.code}`))
 
 
